79226478fa1bab737ec47c3b2984434a39b7cfe2
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-29 23:12:42 -0400

    added a main method to exercise the code

--- utils.scala
+++ utils.scala
@@ -1,9 +1,19 @@
+package utils
 
 class RichInt(n: Int) {
-    def times(body: => Unit) = 1 to n foreach { body() }
+    def times(body: => Unit) = 1 to n foreach { _ => body }
 }
 
-object RichInt(n: Int) {
+object RichInt {
     implicit def int2RichInt(n: Int) = new RichInt(n)
 }
 
+object Main {
+    import RichInt._
+
+    def main(args: Array[String]) {
+        5.times {
+            println("jerk")
+        }
+    }
+}
